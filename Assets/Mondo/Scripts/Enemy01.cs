4ff623f40fa257b681902ed83c08b053f69b08cf
MYCOMPUTER\山城主水 <mondo-y-17e@stu.kbc.ac.jp>
2019-01-30 22:59:12 +0900

    <追加>Assetフォルダー、PlayerController.css、EnemyController.css

---
+++ Assets/Mondo/Scripts/Enemy01.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy01 : MonoBehaviour
+{
+    enum MOVEDIR
+    {
+        LEFT,
+        RIGHT,
+        UP,
+        DOWN,
+    };
+
+    [SerializeField, Header("歩くスピード")]
+    private float walkSpeed;
+    [SerializeField, Header("右の折り返し地点")]
+    private float rightTurningPoint;
+    [SerializeField, Header("左の折り返し地点")]
+    private float leftTurningPoint;
+
+    private MOVEDIR moveDir;                    // キャラが動く方向
+
+    void Start ()
+    {
+        moveDir = MOVEDIR.RIGHT;        // 最初は右へ移動
+	}
+	
+	void Update ()
+    {
+		switch(moveDir)
+        {
+            case MOVEDIR.RIGHT:
+                RightMove();
+                break;
+            case MOVEDIR.LEFT:
+                LeftMove();
+                break;
+        }
+	}
+
+    // 右への移動処理
+    void RightMove()
+    {
+        if(transform.position.x < rightTurningPoint)
+        {
+            transform.position += new Vector3(walkSpeed, 0, 0) * Time.deltaTime;
+        }
+        else
+        {
+            moveDir = MOVEDIR.LEFT;
+        }
+    }
+
+    // 左への移動処理
+    void LeftMove()
+    {
+        if(transform.position.x > leftTurningPoint)
+        {
+            transform.position += new Vector3(-walkSpeed, 0, 0) * Time.deltaTime;
+        }
+        else
+        {
+            moveDir = MOVEDIR.RIGHT;
+        }
+    }
+}
---
+++ Assets/Mondo/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField, Header("プレイヤーの速度")]
+    private float playerSpeed;
+    [SerializeField, Header("回転量")]
+    private float rotationValue;
+
+    private float rotateForce;              // 回る力
+
+	void Start ()
+    {
+	}
+	
+	void Update ()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        transform.position += (new Vector3(horizontal, vertical, 0) * Time.deltaTime) * playerSpeed;
+        PlayerRotate();
+	}
+
+    void PlayerRotate()
+    {
+        if (Input.GetKey(KeyCode.Z))
+        {
+            rotateForce += rotationValue;
+            transform.Rotate(0, 0, rotateForce);
+        }
+        else
+        {
+            rotateForce = 0;
+        }
+    }
+}
