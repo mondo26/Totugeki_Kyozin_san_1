aee527f589b9fc2d485b8d40971a85276d840739
yuuki-s <yuuki-s-17e@stu.kbc.ac.jp>
2019-01-31 16:15:38 +0900

    タイトル

--- Assets/Shinjou/hoge3.cs
+++ Assets/Shinjou/hoge3.cs
@@ -1,16 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class hoge3 : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    [SerializeField]
+    private GameObject _text;
+    [SerializeField]
+    private Text text1;
+    [SerializeField]
+    private GameObject buttonMgr;
+    private bool notcount;
+
+    private float titleStart;
+    private bool SceneEnd_flg;
+
+    public AudioClip checksound;
+    private AudioSource _audioSource;
+
+    void Start ()
+    {
+        _audioSource = gameObject.AddComponent<AudioSource>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    void Update()
+    {
+        if (SceneEnd_flg)
+        {
+            titleStart += Time.deltaTime;
+        }
+        if (titleStart >= 4)
+        {
+            Debug.Log("end");
+            Application.Quit();
+        }
+    }
+    public void HeardButtonClicked()
+    {
+        _audioSource.PlayOneShot(checksound);
+        SceneManager.LoadScene("mainScene");
+    }
+    public void NormalButtonClicked()
+    {
+        _audioSource.PlayOneShot(checksound);
+        SceneManager.LoadScene("");
+    }
+    public void EasyButtonClicked()
+    {
+        _audioSource.PlayOneShot(checksound);
+        SceneManager.LoadScene("");
+    }
+    public void EndButtonClicked()
+    {
+        _audioSource.PlayOneShot(checksound);
+        buttonMgr.SetActive(false);
+        text1.text = "aaaaaaa";
+        SceneEnd_flg = true;
+    }
 }
